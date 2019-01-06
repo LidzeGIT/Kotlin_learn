5be418b4f48c712df659b000c41dfff4e2da4f62
lasamegrelidze <lidzeGit@gmail.com>
2019-01-06 19:32:38 +0300

    Initial commit

---
+++ src/main/kotlin/page/GooglePage.kt
@@ -0,0 +1,55 @@
+package page
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+import org.openqa.selenium.support.ui.WebDriverWait
+import javax.swing.AbstractButton
+
+
+class GooglePage(private val driver: WebDriver) {
+
+    private val pageUrl = "https://www.google.com/"
+
+    init {
+        PageFactory.initElements(driver, this)
+    }
+
+    @FindBy(xpath = "//*[@name='q']")
+    lateinit var findLine: WebElement
+
+    @FindBy(xpath = "//a[@id='gb_70']")
+    lateinit var loginButton : WebElement
+
+    @FindBy(xpath = "//input[@id='identifierId']")
+    lateinit var mailLine: WebElement
+
+    @FindBy(xpath = "//content[@class='CwaK9']//span[text()='Далее']")
+    lateinit var nextButton:WebElement
+
+    @FindBy(xpath = "//div[@class='GQ8Pzc']")
+    lateinit var errorMessage: WebElement
+
+    @FindBy(xpath = "//input[@name='password']")
+    lateinit var passwordLine: WebElement
+
+    @FindBy(xpath = "//div[@class='FPdoLc VlcLAe']//*[@value='Поиск в Google']")
+    lateinit var findGoogle: WebElement
+
+    @FindBy(xpath = "//div[@class='FPdoLc VlcLAe']//*[@value='Мне повезёт!']")
+    lateinit var findLucky: WebElement
+
+    @FindBy(xpath = "//div[@class='gb_Q gb_R']//a[@data-pid='23']")
+    lateinit var mailButton: WebElement
+
+    @FindBy(xpath = "//div[@class='gb_Q gb_R']//a[@data-pid='2']")
+    lateinit var pictureButton: WebElement
+
+
+    fun open() = driver.get(pageUrl)
+
+    fun verifyUrl() {
+        WebDriverWait(driver, 10).until { it.currentUrl == pageUrl }
+    }
+}
---
+++ src/test/kotlin/GoogleTest.kt
@@ -0,0 +1,36 @@
+import io.kotlintest.specs.StringSpec
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.WebDriverWait
+import page.GooglePage
+import java.util.concurrent.TimeUnit
+
+class GoogleTest : StringSpec() {
+    private val driver: WebDriver = autoClose(WebDriverCloseable(ChromeDriver()))
+    private val googlePage = GooglePage(driver)
+    private val wait = WebDriverWait(driver, 10)
+
+
+    init {
+        driver.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
+        driver.manage()?.window()?.maximize()
+
+        "Проверка авторизации в Gmail"{
+            googlePage.run {
+                open()
+                verifyUrl()
+                loginButton.click()
+                mailLine.sendKeys("lidzeGit@gmail.com")
+                nextButton.click()
+                passwordLine.sendKeys("fsdsfd")
+                wait.until { nextButton.isDisplayed }
+                nextButton.click()
+                val lol = errorMessage.text
+                lol.equals("Неверный пароль. Повторите попытку или нажмите на ссылку \"Забыли пароль?\", чтобы сбросить его.")
+            }
+        }
+
+        }
+    }
+
+
