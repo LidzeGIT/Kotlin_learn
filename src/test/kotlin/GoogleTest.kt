04bdc49cd2f3f79c945cb6a67f43c9e131f09022
lasamegrelidze <lidzeGit@gmail.com>
2019-01-06 20:01:46 +0300

    fix asserts

--- src/test/kotlin/GoogleTest.kt
+++ src/test/kotlin/GoogleTest.kt
@@ -1,4 +1,6 @@
+
 import io.kotlintest.specs.StringSpec
+import org.junit.gen5.api.Assertions.assertEquals
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.support.ui.WebDriverWait
@@ -16,21 +18,24 @@ class GoogleTest : StringSpec() {
         driver.manage()?.window()?.maximize()
 
         "Проверка авторизации в Gmail"{
-            googlePage.run {
-                open()
-                verifyUrl()
-                loginButton.click()
-                mailLine.sendKeys("lidzeGit@gmail.com")
-                nextButton.click()
-                passwordLine.sendKeys("fsdsfd")
-                wait.until { nextButton.isDisplayed }
-                nextButton.click()
-                val lol = errorMessage.text
-                lol.equals("Неверный пароль. Повторите попытку или нажмите на ссылку \"Забыли пароль?\", чтобы сбросить его.")
-            }
-        }
-
+            authorization()
         }
     }
+
+    fun authorization(){
+        googlePage.run {
+            open()
+            verifyUrl()
+            loginButton.click()
+            mailLine.sendKeys("lidzeGit@gmail.com")
+            nextButton.click()
+            passwordLine.sendKeys("fsdsfd")
+            wait.until { nextButton.isDisplayed }
+            nextButton.click()
+            val errorDescription = errorMessage.text
+            assertEquals(errorDescription,"Неверный пароль. Повторите попытку или нажмите на ссылку \"Забыли пароль?\", чтобы сбросить его.")
+        }
+    }
+}
 
 
